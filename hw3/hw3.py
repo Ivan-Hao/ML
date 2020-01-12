8c83afdd936e4d7a67c7ddcc31380d18f12eeed7
wangyunhao <wangyunhao@cmlab.csie.ntu.edu.tw>
2020-01-13 00:34:36 +0800

    modify

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -16,6 +16,7 @@ sample_size = train_data.shape[0]
 
 w_initial = np.random.rand(20)-0.5
 S_w_initial = np.copy(w_initial)
+fix_w_initial = np.copy(w_initial)
 
 GD_train_accuracy = []
 GD_train_error = []
@@ -25,8 +26,17 @@ S_train_accuracy = []
 S_train_error = []
 S_train_loss = []
 
-GD_test = [] 
-S_test = []
+GD_test_accuracy = [] 
+S_test_accuracy = []
+f_test_accuracy = []
+GD_test_error = [] 
+S_test_error = []
+f_test_error =[]
+
+
+fix_SGD=[]
+fix_error_SGD=[]
+f_loss=[]
 
 for i in range(epochs):
     '''
@@ -40,6 +50,7 @@ for i in range(epochs):
         gradient = np.tile(np.reshape(gradient,(batch_size,1)),(1,20)) * train_data[front:end,:-1]
         w_initial -= learning_rate*np.average(gradient,axis=0)
     '''
+    f_SGD = i%sample_size
 
     gradient = sigmoid(np.dot(train_data[:,:-1],w_initial)*(-train_data[:,-1]))
     gradient = gradient * (-train_data[:,-1]) 
@@ -50,17 +61,32 @@ for i in range(epochs):
     S_gradient = sigmoid(np.dot(train_data[SGD:SGD+1,:-1],S_w_initial)*(-train_data[SGD:SGD+1,-1]))
     S_gradient = S_gradient * (-train_data[SGD:SGD+1,-1]) * train_data[SGD:SGD+1,:-1]
     S_w_initial -= learning_rate*np.average(S_gradient,axis=0)
+
+    f_gradient = sigmoid(np.dot(train_data[f_SGD:f_SGD+1,:-1],fix_w_initial)*(-train_data[f_SGD:f_SGD+1,-1]))
+    f_gradient = f_gradient * (-train_data[f_SGD:f_SGD+1,-1])  * train_data[f_SGD:f_SGD+1,:-1]
+    fix_w_initial -= learning_rate*np.average(f_gradient,axis=0)
+
+    #fSGD
+    f_y_predict = np.where(sigmoid(np.dot(train_data[:,:-1],fix_w_initial)) >0.5, 1 ,-1)
+    f_precision = np.average(f_y_predict == train_data[:,-1])
+    f_error = 1-f_precision
         
 
     #GD
     y_predict = np.where(sigmoid(np.dot(train_data[:,:-1],w_initial)) >0.5, 1 ,-1)
     precision = np.average(y_predict == train_data[:,-1])
-    error = np.average(y_predict != train_data[:,-1])
+    error = 1-precision
 
     #SGD
     S_y_predict = np.where(sigmoid(np.dot(train_data[:,:-1],S_w_initial)) >0.5, 1 ,-1)
     S_precision = np.average(S_y_predict == train_data[:,-1])
-    S_error = np.average(S_y_predict != train_data[:,-1])
+    S_error = 1-S_precision
+
+    #fSGD
+    fix_SGD.append(f_precision)
+    fix_error_SGD.append(f_error)
+    fix_loss = np.average(crossentropy(train_data[:,:-1],train_data[:,-1],fix_w_initial))
+    f_loss.append(fix_loss) 
 
 
     #GD
@@ -75,15 +101,28 @@ for i in range(epochs):
     S_train_accuracy.append(S_precision)
     S_train_error.append(S_error)
 
+
+    #fSGD
+    f_out_precision = np.where(sigmoid(np.dot(test_data[:,:-1],fix_w_initial)) >0.5, 1 ,-1)
+    f_out_acc = np.average(f_out_precision == test_data[:,-1])
+    f_out_error = 1-f_out_acc
+    f_test_accuracy.append(f_out_acc)
+    f_test_error.append(f_out_error)
+
+
     #GD test
     out_precision = np.where(sigmoid(np.dot(test_data[:,:-1],w_initial)) >0.5, 1 ,-1)
-    out_precision = np.average(out_precision == test_data[:,-1])
-    GD_test.append(out_precision)    
+    out_acc = np.average(out_precision == test_data[:,-1])
+    out_error = 1-out_acc
+    GD_test_accuracy.append(out_acc)
+    GD_test_error.append(out_error)    
 
     #SGD test
     S_out_precision = np.where(sigmoid(np.dot(test_data[:,:-1],S_w_initial)) >0.5, 1 ,-1)
-    S_out_precision = np.average(S_out_precision == test_data[:,-1])   
-    S_test.append(S_out_precision)
+    S_out_acc = np.average(S_out_precision == test_data[:,-1])
+    S_out_error = 1-S_out_acc  
+    S_test_accuracy.append(S_out_acc)
+    S_test_error.append(S_out_error)
 
 
 # GD and loss
@@ -108,35 +147,61 @@ plt.legend(loc=0)
 plt.tick_params(axis='both', color='red')
 plt.show()
 
-#Ein GD vs SGD
+#fSGD and loss
+plt.figure()
+plt.plot(fix_SGD,'g-',label="fSGD accuracy" )
+plt.plot(fix_error_SGD,'b-', label = "fSGD loss")
+plt.title("Training precision and error rate")
+plt.xlabel("T")
+plt.ylabel("value")
+plt.legend(loc=0)
+plt.tick_params(axis='both', color='red')
+plt.show()
+
+#Ein GD vs SGD vs fSGD
 plt.figure()
 plt.plot(S_train_error,'g-', label = "SGD")
 plt.plot(GD_train_error,'b-', label = "GD")
-plt.title("Ein GD and SGD")
+plt.plot(fix_error_SGD,'r-', label="fSGD")
+plt.title("Ein GD and SGD and fSGD")
 plt.xlabel("T")
 plt.ylabel("Ein")
 plt.legend(loc=0)
 plt.tick_params(axis='both', color='red')
 plt.show()
 
-#acc GD and SGD
+#acc GD and SGD and fSGD
 plt.figure()
 plt.plot(GD_train_accuracy,'g-',label="GD accuracy" )
 plt.plot(S_train_accuracy,'b-', label = "SGD accuracy")
-plt.title("GD and SGD accuracy")
+plt.plot(fix_SGD,'r-', label = "fSGD accuracy")
+plt.title("GD and SGD and fSGD accuracy")
 plt.xlabel("T")
 plt.ylabel("accuracy")
 plt.legend(loc=0)
 plt.tick_params(axis='both', color='red')
 plt.show()
 
-#test GD and SGD
+#test GD and SGD and fSGD
 plt.figure()
-plt.plot(GD_test,'g-',label="test GD accuracy" )
-plt.plot(S_test,'b-', label = "test SGD accuracy")
-plt.title("Test GD and SGD accuracy")
+plt.plot(GD_test_accuracy,'g-',label="test GD accuracy" )
+plt.plot(S_test_accuracy,'b-', label = "test SGD accuracy")
+plt.plot(f_test_accuracy,'r-', label = "test fSGD accuracy")
+plt.title("Test GD and SGD and fSGD accuracy")
 plt.xlabel("T")
 plt.ylabel("accuracy")
+plt.legend(loc=0)
+plt.tick_params(axis='both', color='red')
+plt.show()
+
+#test GD and SGD and fSGD
+plt.figure()
+plt.plot(GD_test_error,'g-',label="test GD error" )
+plt.plot(S_test_error,'b-', label = "test SGD error")
+plt.plot(f_test_error,'r-', label = "test fSGD error")
+plt.title("Eout")
+plt.xlabel("T")
+plt.ylabel("Eout")
 plt.legend(loc=0)
 plt.tick_params(axis='both', color='red')
 plt.show()
